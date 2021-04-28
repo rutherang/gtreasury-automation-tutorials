8f63ca499b8b9a292d7a3a70b6037f1f9b8efd89
Ruther Ford Myler Ang <ruther.ang@outlook.com>
2021-04-28 09:33:54 +0800

    Add Lesson 3 - Cypress tutorial

---
+++ Lesson3.Cypress/e2e/login.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe("login module", () => {
+  it("login as standard user", () => {
+    cy.visit("");
+    cy.getByTestId("username").type("standard_user");
+    cy.getByTestId("password").type("secret_sauce");
+    cy.getByTestId("login-button").click();
+    cy.get("span.title").should("have.text", "products");
+  });
+
+  it("login as lockout user", () => {
+    cy.visit("");
+    cy.getByTestId("username").type("locked_out_user");
+    cy.getByTestId("password").type("secret_sauce");
+    cy.getByTestId("login-button").click();
+
+    cy.getByTestId("error").should("have.text", "Epic sadface: Sorry, this user has been locked out.");
+  });
+});
